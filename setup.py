1bdb6dc8b244b149e166013da9ffb53137650228
zhouzhong <1097449124@qq.com>
2021-11-03 15:15:47 +0800

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/FudanSELab/kgdt',
-    version='0.4.0',
+    version='0.4.1',
     zip_safe=False,
 )
